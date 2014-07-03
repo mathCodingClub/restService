2f8d44ab65b98ec161d06ce6c01072c43d7a9b56
Asser Lahdemaki <asser@lahdemaki.fi>
2014-07-04 00:09:42 +0300

    Added lotto service and acce testing variables to config

---
+++ service/lotto/Lotto.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WS;
+
+require_once PATH_GITHUB . 'mathCodingClub/lotto/Lotto.php';
+
+use \WS\annotations as WSann;
+
+class Lotto extends service {
+
+  private $lotto;
+
+  public function __construct($app, $path) {
+    parent::__construct($app, $path);
+  }
+
+  /**
+   * @WSann\HelpTxt("Returns latest lotto numbers")
+   */
+  public function get() {
+    $this->lotto = new \Lotto();
+    $this->setCT(self::CT_PLAIN);
+    $this->response->body($this->lotto->get());
+  }
+
+  /**
+   * @WSann\HelpTxt("takes 7 numbers and tells how many match latest numbers")
+   */
+  public function getLatestMatch($nro1,$nro2,$nro3,$nro4,$nro5,$nro6,$nro7) {
+    $this->lotto = new \Lotto();
+    $this->setCT(self::CT_PLAIN);
+    $this->response->body($this->lotto->getLatestMatch($nro1,$nro2,$nro3,$nro4,$nro5,$nro6,$nro7));
+  }
+
+}
+
+?>
